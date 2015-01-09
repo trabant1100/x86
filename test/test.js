b9bd9b8bc56c3590b316f8999146894f571f7ee2
Kam Om <trabant1100@outlook.com>
2015-01-09 18:17:19 +0100

    Initial mocha test config

---
+++ test/test.js
@@ -0,0 +1,25 @@
+var assert = require('assert'),
+	fs = require('fs'),
+	io = require('socket.io-client');
+
+var bios = null;
+
+describe('x86', function() {
+	var socket = null;
+
+	beforeEach(function(done) {
+		fs.readFile('./test/xtbios.bin', function(err, buf) {
+			if(err) 
+				throw err;
+			// console.info('loaded bios', buf);
+			bios = buf;
+			done();
+		});
+	});
+
+	describe('x86 test', function() {
+		it('should get bios data', function() {
+			assert.equal(true, bios != null);
+		});
+	});
+});
