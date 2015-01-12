3c19b2a97ab808670a37d0ca722ad5f0f1f546e5
Kam Om <trabant1100@outlook.com>
2015-01-12 22:42:33 +0100

    Loading csv asserts

--- test/test.js
+++ test/test.js
@@ -1,18 +1,51 @@
-var assert = require('assert'),
+var _ = require('lodash'),
+	assert = require('assert'),
 	fs = require('fs'),
+	csv = require('csv'),
+	Promise = require('promise'),
 	x86 = require('../src/x86');
 
-var bios = null;
+var bios = null,
+	ass = null;
 
 describe('x86', function() {
-	var socket = null;
-
 	before(function(done) {
-		fs.readFile('./test/xtbios.bin', function(err, buf) {
-			if(err) 
-				throw err;
-			// console.info('loaded bios', buf);
+		function readBios(filename) {
+			return new Promise(function(resolve, reject) {
+				fs.readFile(filename, function(err, buf) {
+					if(err)
+						reject(err);
+					else
+						resolve(buf);
+				});
+			});
+		}
+
+		function readAsserts(filename) {
+			return new Promise(function(resolve, reject) {
+				var readStream = fs.createReadStream(filename, {flags: 'r', encoding: 'utf8'});
+				var parser = csv.parse(), ass = [];
+
+				try {
+					readStream.pipe(csv.parse({delimiter: '\t', columns: true, auto_parse: true}))
+						.pipe(csv.transform(function(record) {
+							ass.push(_.mapValues(record, function(value) {
+								return value === '' ? null : value;
+							}));
+						})).on('finish', function() {
+							resolve(ass);
+						});
+				} catch(e) {
+					reject(e);
+				}
+			});
+		}
+
+		readBios('./test/xtbios.bin').then(function(buf) {
 			bios = buf;
+			return readAsserts('./test/asserts.csv');
+		}).then(function(ass) {
+			// console.info(ass);
 			done();
 		});
 	});
