da9ee2677816fcfcdbbac44ea112ed974999a8e0
Kam Om <trabant1100@outlook.com>
2015-01-13 21:05:08 +0100

    Fixed writing bios into memory

--- src/mem.js
+++ src/mem.js
@@ -41,7 +41,13 @@ var mem = (function() {
 			for(i = 0, len = data.length; i < len; i ++) {
 				if(addr + i > ram.length)
 					throw 'End of memory';
-				ram[addr + i] = (data[i] + '').charCodeAt(0);
+
+				if (typeof data[i] == 'number')
+					ram[addr + i] = data[i];
+				else if(typeof data[i] == 'string')
+					ram[addr + i] = data[i].charCodeAt(0);
+				else
+					throw 'Unsupported mem write type: ' + (typeof data[i]);
 			}
 		},
 		
